d46bd4ecb6b001d64f121bb935d5df21215c5ee6
narayanacharyuluchitroju <raghava.chitroju@gmail.com>
2024-10-13 18:58:09 -0400

    Resolved merge conflicts

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -3,11 +3,19 @@ import React from 'react';
 
 const Certifications = () => {
   return (
-    <section>
+    <section className="certifications-section">
       <h2>Certifications</h2>
       <ul>
-        <li>Oracle Cloud Infrastructure 2024 Generative AI Certified Professional</li>
-        <li>Oracle Database SQL Certified Associate</li>
+        <li>
+          <a href="https://catalog-education.oracle.com/ords/certview/sharebadge?id=CDE22CA31096BDC5BA53FB79DC3C6D5CF9AAC8E77EF73A6FC5B2935917DECB61" target="_blank" rel="noopener noreferrer">
+            Oracle Cloud Infrastructure 2024 Generative AI Certified Professional
+          </a>
+        </li>
+        <li>
+          <a href="https://catalog-education.oracle.com/ords/certview/sharebadge?id=AEBEFF1D8CFCD32B8DFB0C19CABA2BEE5E6038A3F8431D187683E1DF2B40063E" target="_blank" rel="noopener noreferrer">
+            Oracle Database SQL Certified Associate
+          </a>
+        </li>
       </ul>
     </section>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,47 @@
-// src/components/Certifications.js
+// src/components/Skills.js
 import React from 'react';
 
-const Certifications = () => {
+
+const Skills = () => {
   return (
-    <section>
-      <h2>Certifications</h2>
-      <ul>
-        <li>Oracle Cloud Infrastructure 2024 Generative AI Certified Professional</li>
-        <li>Oracle Database SQL Certified Associate</li>
-      </ul>
+    <section className="skills-section">
+      <h2>Skills</h2>
+      <div className="skills-container">
+        <div className="skill-card">
+          <h3>Programming</h3>
+          <p>Java, Python, C++, Linux Shell Scripting, SQL, NoSQL</p>
+        </div>
+        <div className="skill-card">
+          <h3>Application Frameworks</h3>
+          <p>Spring Boot, React Js, Django</p>
+        </div>
+        <div className="skill-card">
+          <h3>Big Data Technologies</h3>
+          <p>Apache Spark, PySpark, Kafka</p>
+        </div>
+        <div className="skill-card">
+          <h3>Cloud Platforms</h3>
+          <p>Amazon Web Services (AWS), Microsoft Azure</p>
+        </div>
+        <div className="skill-card">
+          <h3>Machine Learning & AI Frameworks</h3>
+          <p>PyTorch, TensorFlow, Scikit-Learn, Hugging Face, MLflow, Keras</p>
+        </div>
+        <div className="skill-card">
+          <h3>ETL Development</h3>
+          <p>Informatica PowerCenter, IBM DataStage, Control M, Toad, SQL Developer</p>
+        </div>
+        <div className="skill-card">
+          <h3>Tools</h3>
+          <p>Git, Jenkins, Docker, Kubernetes, Postman</p>
+        </div>
+        <div className="skill-card">
+          <h3>Data Governance</h3>
+          <p>Data Quality, Metadata Management, Data Security</p>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Certifications;
+export default Skills;
