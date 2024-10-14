90e9129d37cf5aad7f874af20e95b5dabaa866dc
narayanacharyuluchitroju <raghava.chitroju@gmail.com>
2024-10-14 10:19:56 -0400

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// src/App.js
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Education from './components/Education';
@@ -7,35 +6,19 @@ import WorkExperience from './components/WorkExperience';
 import Projects from './components/Projects';
 import Certifications from './components/Certifications';
 import Skills from './components/Skills';
-import ContactForm from './components/ContactForm'; // Import the ContactForm component
 
 function App() {
-  const [formSubmitted, setFormSubmitted] = useState(false); // State to track form submission
-
-  const handleFormSubmit = () => {
-    setFormSubmitted(true); // Set form submission state to true
-  };
-
   return (
     <div className="container">
-      {!formSubmitted ? (
-        <div className="contact-form-section">
-          <h2>Please provide your details to access the portfolio</h2>
-          <ContactForm onSubmit={handleFormSubmit} /> {/* Pass the handleFormSubmit function */}
-        </div>
-      ) : (
-        <>
-          <Header />
-          <Education />
-          <WorkExperience />
-          <Projects />
-          <Certifications />
-          <Skills />
-          <footer>
-            &copy; 2024 Raghavendra Chitroju
-          </footer>
-        </>
-      )}
+      <Header />
+      <Education />
+      <WorkExperience />
+      <Projects />
+      <Certifications />
+      <Skills />
+      <footer>
+        &copy; 2024 Raghavendra Chitroju
+      </footer>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <h1>Raghavendra Chitroju</h1>
       <p>Email: <a href="mailto:raghava.chitroju@gmail.com">raghava.chitroju@gmail.com</a></p>
-      <p>Phone: 937-304-1189</p>
+      <p>Phone: 937-304-1179</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/ksars/">https://www.linkedin.com/in/ksars/</a></p>
       <p>GitHub: <a href="https://github.com/narayanacharyuluchitroju">https://github.com/narayanacharyuluchitroju</a></p>
     </header>
